d08fd8e1aa0512ab490c257252a0615b31971048
Владислав Яцун <challenger@zeocamp-29.zeo.lcl>
2016-08-19 19:32:15 +0300

    init desktop project

---
+++ desktop-side/ImageEditor/ImageEditor/CHDocument.h
@@ -0,0 +1,15 @@
+//
+//  Document.h
+//  ImageEditor
+//
+//  Created by Владислав Яцун on 8/19/16.
+//  Copyright © 2016 Владислав Яцун. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CHDocument : NSDocument
+
+
+@end
+
---
+++ desktop-side/ImageEditor/ImageEditor/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ImageEditor
+//
+//  Created by Владислав Яцун on 8/19/16.
+//  Copyright © 2016 Владислав Яцун. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
